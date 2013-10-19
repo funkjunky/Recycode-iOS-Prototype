fba0dd3fef34df73c9f795aa306dbd039195b499
Jason McCarrell <jason.abz@gmail.com>
2013-10-19 14:06:02 -0400

    First.

---
+++ examples/BarcodeScanner/recycode/ProductDetails.h
@@ -0,0 +1,13 @@
+//
+//  ProductDetails.h
+//  BarcodeScanner
+//
+//  Created by Angel Genov on 10/19/13.
+//  Copyright (c) 2013 Draconis Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProductDetails : UIView
+
+@end
---
+++ examples/BarcodeScanner/recycode/ProductDetails.m
@@ -0,0 +1,31 @@
+//
+//  ProductDetails.m
+//  BarcodeScanner
+//
+//  Created by Angel Genov on 10/19/13.
+//  Copyright (c) 2013 Draconis Software. All rights reserved.
+//
+
+#import "ProductDetails.h"
+
+@implementation ProductDetails
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
